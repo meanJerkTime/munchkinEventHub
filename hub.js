625e72638de0082570c5c9d2d8fb6dae58a6f4fb
Joe Pennock <pennockjoe@gmail.com>
2020-10-21 20:31:08 -0700

    reworked socket connection to run off of a gameroom namespace, confirmed connection with client

--- hub.js
+++ hub.js
@@ -2,7 +2,7 @@
 
 /** 3rd party dependencies */
 require('dotenv').config();
-const io = require('socket.io')(3000); // <-- Connected to whichever server is hosting events
+const io = require('socket.io')(3000); // Connected to whichever server is hosting events
 
 /** Primary game namespace */
 const gameRoom = io.of('/gameroom');
@@ -13,7 +13,7 @@ io.engine.generateId = (req) => {
   return "User:" + custom_id + Math.floor(Math.random() * 10000);
 };
 
-io.on('connect', (socket) => {
+gameRoom.on('connect', (socket) => {
   
   socket.on('fromClient', () => {
     console.log(socket.id, 'Connected');
@@ -31,10 +31,53 @@ io.on('connect', (socket) => {
   // joins the room
   socket.on('joinRoom', function(room) {
     socket.join(room);
-    // console.log(socket.id, 'joined Room', room);
+    console.log(socket.id, 'created Room', room);
   });
   console.log(socket.adapter.rooms);
+
 });
+
+gameRoom.on('connect', socket => {
+  
+  socket.emit('game-start')
+
+});
+
+
+
+/* 
+
+BASIC TURN ORDER
+
+1. players joins a room
+2. game starts
+3. players roll for turn order
+4. P1 kicks down door
+5. Is it a monster? 
+  i. combat starts
+  ii. if P1 can beat monster:
+    a. P1 level++
+    b. P1 recieves treasure
+    c. P1 can play any applicable cards
+  iii. if P1 can't beat monster
+    a. ask for help (stretch goal)
+    b. roll d6 to run away
+      1. if roll succeeds, P1 turn is over
+      2. if roll fails, P1 loses combat
+      3. resolve any bad stuff
+6. Is it a curse?
+  i. curse effect applies to P1 immediately
+  ii. P1 can look for trouble or loot the room (see below)
+7. Is it neither?
+  i. P1 can look for trouble
+    a. play monster from your hand, standard combat rules apply
+  ii. P1 can loot the room
+    a. face down door card goes into P1s hand
+8. P1 plays any applicable cards i.e. equipment, curses against other players etc
+9. P1 turn is over, P2 turn start
+10. Repeat from step 1
+
+*/
  
 
 
