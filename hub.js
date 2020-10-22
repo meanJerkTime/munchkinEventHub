618753410cd87695c9cf3c950b529c5abd8eddb9
BrendonLH <Brendonhampton1988@gmail.com>
2020-10-22 15:52:19 -0700

    fixed server event hub to post to the server api

--- hub.js
+++ hub.js
@@ -1,25 +1,12 @@
 'use strict';
 require('dotenv').config();
+const superagent = require('superagent');
 
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-
 const io = require('socket.io')(http);
 
-// connected to the server
-const ioClient = require('socket.io-client');
-
-const socket = ioClient.connect('https://munchkin-401-server.herokuapp.com');
-
-socket.on('hubConnected', () => {
-  console.log('server and hub connected');
-});
-
-
-
-/** Primary game namespace */
-const gameRoom = io.of('/gameroom');
 
 /** Built-in method of socket.io engine to overwrite default socket ID and create a custom ID for all connected sockets */
 let custom_id = 0;
@@ -29,25 +16,28 @@ io.engine.generateId = (req) => {
 
 io.on('connection', (socket) => {
   
+  
   socket.on('fromClient', () => {
     console.log(socket.id, 'Connected');
       socket.emit('toClient');
   });
-  socket.on('answers', (payload) => {
-    // console.log(payload.roomsList);
-  });
-
   // on create room also joins the room
   socket.on('createRoom', function(room) {
     socket.join(room);
     console.log(socket.id, 'joined Room', room);
   });
   // joins the room
-  socket.on('joinRoom', function(room) {
-    socket.join(room);
-    // console.log(socket.id, 'joined Room', room);
+  socket.on('signUp', function(user) {
+    superagent.post('http://localhost:3000/signup')
+      .send({username:user.userName, password:user.password})
+      .set('X-API-Key', 'foobar')
+      .set('accept', 'json')
+      .end((err, res) => {
+      });
+    console.log(user);
+
   });
-  console.log(socket.adapter.rooms);
+  
 });
  
 
